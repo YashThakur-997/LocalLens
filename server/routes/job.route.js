5436d62543d1fe1873ce9de05140ad7697966631
Yash Thakur <yashthakur99755@gmail.com>
2026-04-20 00:36:27 +0530

    feat(auth): add JWT authentication middleware and apply it to job routes

--- server/routes/job.route.js
+++ server/routes/job.route.js
@@ -1,11 +1,12 @@
 let router = require('express').Router();
 let { requestCompletion, verifyAndRate } = require('../controllers/otp.validation');
+let authMiddleware = require('../middlewares/auth.token');
 let { createJob } = require('../controllers/job.controller');
 
-router.post('/create', createJob);
+router.post('/create', authMiddleware, createJob);
 
-router.post('/request-completion/:jobId', requestCompletion);
+router.post('/request-completion/:jobId', authMiddleware, requestCompletion);
 
-router.post('/verify-and-rate', verifyAndRate);
+router.post('/verify-and-rate', authMiddleware, verifyAndRate);
 
 module.exports = router;
